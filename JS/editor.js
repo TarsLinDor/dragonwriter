1a43946a07471c0ab5aeb1b701b3b68f3e0c80d6
TarsLinDor <thory.vandyke@gmail.com>
2021-01-28 22:11:56 -0500

    2 files modified, 1 added, 1 deleted

--- JS/editor.js
+++ JS/editor.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import $ from 'jquery-ui';
+//import "//.CSS/editor.css";
 
 
 var toolbarOptions = [
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import stylesheets
 import "./CSS/DragonWriter.css";
-import "./CSS/editor.css";
+//import "./CSS/editor.css";
 import "./JS/editor.js";
 
 
