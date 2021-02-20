3f99306089ce6b0783d7b8d3f6afa308f7fbecd9
TarsLinDor <thory.vandyke@gmail.com>
2021-02-19 20:44:41 -0500

    1 file modified

--- index.js
+++ index.js
@@ -35,7 +35,10 @@ async function user_login() {
     credentialHelper: firebaseui.auth.CredentialHelper.NONE,
     signInOptions: [
       // Email / Password Provider.
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
       signInSuccessWithAuthResult: function(authResult, redirectUrl) {
