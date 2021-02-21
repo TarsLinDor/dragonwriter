328342c351772db77f11fed2d30d77b23e7e03ba
TarsLinDor <thory.vandyke@gmail.com>
2021-02-20 22:19:19 -0500

    3 files modified

--- index.js
+++ index.js
@@ -1,5 +1,121 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import * as firebaseui from "firebaseui";
+
+//Define all global variable here
+const login = document.getElementById('login');
+const logout = document.getElementById('logout');
+const bookTitle = document.getElementById('bookTitle');
+const bookName = document.getElementById('BookName');
+const addBook = document.getElementById('addBook');
+
+//run main function
+main();
+
+async function main() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyC8YOMLaOiD72p4i5DYRSAFwQB7B0AO9vE",
+    authDomain: "dragonwriter-2d4d4.firebaseapp.com",
+    projectId: "dragonwriter-2d4d4",
+    storageBucket: "dragonwriter-2d4d4.appspot.com",
+    messagingSenderId: "986346360064",
+    appId: "1:986346360064:web:552c83759e18a086e0b7e5",
+    measurementId: "G-6VYBWWEX41"
+  };
+  firebase.initializeApp(firebaseConfig);
+  login_func(firebase);
+  newbook(firebase);
+};
+
+async function login_func(firebase){
+  // Add Firebase project configuration object here
+  var firebaseConfig = {};
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // Handle sign-in.
+        console.log("Login successfull!");
+        
+
+        // Return false to avoid redirect.
+        return false;
+      }
+    }
+  };
+
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  ui.start('#login_window', uiConfig);
+
+   // Listen to the current Auth state
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+    login.style.display ='none';
+    }
+    else {
+    login.style.display ='grid';
+    }
+  });
+};
+
+async function newbook(firebase) {
+  var db = firebase.firestore();
+  addBook.addEventListener('click', 
+  function(){
+    var newbook = db.collection("book").doc();
+    if(bookName == null){
+    
+    
+    newbook.set({
+        title: "Book Title",
+        genre: "Fantasy",
+        lenght: "Short Story",
+        perspective: '3rd Person',
+        audience: 'YA',
+        tags: [null]
+    })
+    .then(() => {
+      console.log("Document successfully written!");
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+    }
+    else{
+          
+    
+    newbook.set({
+        title: bookName,
+        genre: "Fantasy",
+        lenght: "Short Story",
+        perspective: '3rd Person',
+        audience: 'YA',
+        tags: [null]
+    })
+    .then(() => {
+      console.log("Document successfully written!");
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+
+    };
+  });
+
+
+};
+
 import './style.css';
-import "./tools/login/login.js"
 import "./tools/toolbar/toolbar.js"
 import "./tools/editor/editor.js"
-import "./tools/bookmenu/bookmenu.js"
+
+
