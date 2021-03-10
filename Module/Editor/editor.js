d6bf97adcd11ac8ec7c53fb0ad522cf7c7def081
TarsLinDor <thory.vandyke@gmail.com>
2021-03-09 23:17:53 -0500

    2 files modified

--- Module/Editor/editor.js
+++ Module/Editor/editor.js
@@ -26,6 +26,49 @@ var bookid = localStorage.getItem('bookid');
 var booktitle = localStorage.getItem('booktitle');
 
 firebase.auth().onAuthStateChanged((user) => { if(user){// all functions should be done only if user is logged in
+  db.collection("books").doc(bookid).collection('contents').orderBy('order')
+    .onSnapshot((snaps) => {
+            // Reset page
+            $("#content-list").html('');
+            // Loop through documents in database
+              snaps.forEach((doc) => {
+                if(doc.data().type == 'Chapter'){
+                  var item = "<li class = 'leftmenu-list' id ='"+doc.id+"'>\
+                              <a class='content_title'>"+doc.data().title+"</a>\
+                              <i class='fas fa-chevron-down dropdown'></i>\
+                              </li>";
+                }
+                else{
+                  var item = "<div class = 'leftmenu-list' id ='"+doc.id+"'>\
+                              <a class='content_title'>"+doc.data().type+": "+doc.data().title+"</a>\
+                              <i class='fas fa-chevron-down dropdown'></i>\
+                              </div>";
+                };
+              $("#content-list").append(item);
+              });
+        });
+  $(document).on('click','#AddContent', function(){
+          const bookid = localStorage.getItem('bookid');
+            if(bookid != null){
+              firebase.firestore().collection("books").doc(bookid).collection('contents').add({
+                  bookId: bookid,
+                  timestamp: Date.now(),
+                  title: "Title",
+                  type: 'Chapter',
+                  pov: "none",
+                  discription: "Write a Chapter Discription.",
+                  draft: 1,
+                  order: $('#content-list').children().length,
+                  content: ['test', 'draft 2'],
+                      })
+                .then(() => {
+                  console.log("Document successfully written!");
+                  })
+                .catch((error) => {
+                  console.error("Error writing document: ", error);
+                  });
+              };
+  });
 
 };
 });
