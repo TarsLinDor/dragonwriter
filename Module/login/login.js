4f498fdf122607ce2e95acd23b4470f060303bd4
TarsLinDor <thory.vandyke@gmail.com>
2021-03-29 20:54:10 -0400

    4 files modified

--- Module/login/login.js
+++ Module/login/login.js
@@ -3,61 +3,51 @@ import "firebase/auth";
 import "firebase/firestore";
 import * as firebaseui from "firebaseui";
 import $ from "jquery";
-import './login.css';
+import "./login.css";
 
+function login_logout() {
+  // Logs users in and out of DragonWriter.
 
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+    ],
+    tosUrl: "<your-tos-link>",
+    privacyPolicyUrl: "<your-privacyPolicyUrl-link>",
+    callbacks: {
+      signInSuccessWithAuthResult: function() {
+        // Handle sign-in.
+        console.log("sign in successfull");
+        return false;
+      }
+    }
+  };
 
-/*$(document).ready(function() {
-  $("body").load('./login.html');
-});
-*/
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  ui.start("firebaseUI-login", uiConfig);
 
-$( "login" ).get( "./login.html .welcome" );
+  // Listen to the current Auth state
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      $("login").hide();
+    } else {
+      $("login").show();
+    }
+  });
 
+  $("#logout").on("click", function() {
+    if (firebase.auth().currentUser) {
+      // User is signed in, let's sign out
+      firebase.auth().signOut();
+      console.log("Logged out successfully!");
+    }
+  });
+}
 
-async function login_logout(){ // Logs users in and out of DragonWriter.
-
-      const uiConfig = {
-        credentialHelper: firebaseui.auth.CredentialHelper.NONE,
-        signInOptions: [
-          // Email / Password Provider.
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-        ],
-          tosUrl: '<your-tos-link>',
-          privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
-          callbacks: {
-          signInSuccessWithAuthResult: function() {
-            // Handle sign-in.
-            console.log('sign in successfull');
-            return false;
-          }
-        }
-      };
-
-      const ui = new firebaseui.auth.AuthUI(firebase.auth());
-      ui.start('firebaseUI-login', uiConfig);
-
-      // Listen to the current Auth state
-      firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-        $('login').hide();
-        }
-        else {
-        $('login').show();
-        }
-      });
-
-      $('#logout').on('click', function(){
-        if (firebase.auth().currentUser) {
-          // User is signed in, let's sign out
-          firebase.auth().signOut();
-          console.log("Logged out successfully!");
-        } 
-      });
-};
-
-login_logout();
+export default login_logout;
--- index.js
+++ index.js
@@ -6,28 +6,35 @@
   import './style.css';
   import $ from "jquery";
   import './Module/firebase/startfire.js';
-  import './Module/login/login.js';
+  import login_logout from './Module/login/login.js';
   import './Module/toolbar/toolbar.js';
   import './Module/Editor/editor.js';
-  import  './Module/BookMenu/bookmenu.js';
-  import  './Module/world/world.js';
+  import './Module/BookMenu/bookmenu.js';
+  import './Module/world/world.js';
   import bookmenu from './Module/BookMenu/bookmenu.html';
   import toolbar from './Module/toolbar/toolbar.html';
   import editor from './Module/Editor/editor.html';
+  import login from './Module/login/login.html';
 //end of imports 
 
-   
-// Load App and set initial conditions. 
-$(document).ready(function() { // Loads App and establishes base load state.
+$(document).ready(function() { 
     console.log( "ready!" );
+    $('login').html(login);
+    login_logout();
     $('booklist-menu').html(bookmenu);
     $('toolbar').html(toolbar);
     $('editor').html(editor);
-    editor;
-    world;
   });
 
-// load contents on click
+   $("#logout").on("click", function() {
+    if (firebase.auth().currentUser) {
+      // User is signed in, let's sign out
+      firebase.auth().signOut();
+      console.log("Logged out successfully!");
+    }
+  }); 
+
+
 
 
 
