4e13dde6d67e302f3eeffa75c89cc5536e34a4f5
TarsLinDor <thory.vandyke@gmail.com>
2021-04-01 12:09:06 -0400

    2 files modified, 3 added, 1 deleted

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+import $ from 'jquery';
 import 'firebase/auth';
 import 'firebase/firestore';
 import './Module/firebase/startfire.js';
 import './style.css';
-import $ from 'jquery';
 import './Module/toolbar/toolbar.js';
 import './Module/BookMenu/bookmenu.js';
 import './Module/Editor/editor.js';
 import './Module/world/world.js';
 import './Module/login/login.js';
 
-$(document).ready(function(){
+$(document).ready(function(){ //initial load technically should be done after login so needs to be moved.
   $('book').first().children('booktitle').trigger('click');
   $('#editor').trigger('click');
 });
